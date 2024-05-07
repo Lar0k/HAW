f971b0bbfc080de95aa3c2b5b68926d447b70874
Lar0k <167089768+Lar0k@users.noreply.github.com>
2024-05-07 13:16:29 +0000

    Add files via upload

---
+++ a2x1/a2x1/src/simplifiedBlackJack/TestFrame.java
@@ -0,0 +1,11 @@
+package simplifiedBlackJack;
+
+
+public class TestFrame {
+    
+    public static void main( final String... unused ) {
+        SimplifiedBlackJackAgent simplifiedBlackJackAgent = new SimplifiedBlackJackAgent();
+        simplifiedBlackJackAgent.playBlackJack();
+    }//method()
+    
+}//class
